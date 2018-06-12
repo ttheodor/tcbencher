b9058494d51e279a500041ef004d1e2d8ad5274a
Theodoros Theodoridis <theodoridisgr@gmail.com>
2018-06-12 16:59:35 +0200

    Make dir setable in regen_cmake.sh

--- src/cuda_sources/regen_cmake.sh
+++ src/cuda_sources/regen_cmake.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR=${DIR:=${SOURCE_DIR}}
 sources=$(ls ${DIR}/*cu)
 
-printf 'add_library(kernels STATIC '"${sources}"')\ntarget_include_directories(kernels PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/..)\nset_target_properties(kernels PROPERTIES CUDA_SEPARABLE_COMPILATION ON)\n' > ${DIR}/CMakeLists.txt
+printf 'add_library(kernels STATIC '"${sources}"')\ntarget_include_directories(kernels PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/..)\nset_target_properties(kernels PROPERTIES CUDA_SEPARABLE_COMPILATION ON)\n' > ${SOURCE_DIR}/CMakeLists.txt
